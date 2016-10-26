68f078375a9f03283b8e4e50419c1e6aa90a159b
Krzysztof Grygiel <Krzysztof Grygiel>
2016-10-27 00:23:57 +0200

    First commit

---
+++ RussianPeasantAlgorithm.php
@@ -0,0 +1,24 @@
+<?php
+
+function russian($a,$b){
+    $x = $a; 
+	$y = $b;
+    $z = 0;
+		while ($x > 0) {
+        if ($x % 2 == 1) 
+		$z = $z + $y;
+		$y = $y << 1;
+        $x = $x >> 1;
+		}
+    return $z;
+}
+
+$time_start = microtime(true);
+
+echo "Wynik: ".russian(20,7)." dla liczb 20,7";
+
+$time_end = microtime(true);
+$time = $time_end - $time_start;
+$human = date("H:i:s",$time_end-$time_start);
+echo "<br>Algorytm zadziałał w czasie ".number_format($time, 6)." mikrosekund<br>To jest w czasie: ".$human." h/m/s";
+?>
---
+++ eukl.php
@@ -0,0 +1,18 @@
+<?php
+//Algorytm Euklidesa
+
+function nwd($a, $b){
+    
+    if ( $b == 0 ) {
+        
+        return $a;
+        
+    }else{
+        
+        return nwd($b, $a % $b); // operator % w php realizuje funkcję modulo
+    }
+
+} // (reszta z dzielenia liczb całkowitych)
+
+echo "nwd(122,16) = ".nwd(122,16);
+?>
